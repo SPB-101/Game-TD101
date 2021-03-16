fe56c34f9be2f0b51504b200ae4fa82895b17470
Nikita Stroganov <findoss@yandex.ru>
2021-03-16 23:45:55 +0300

    diff

---
+++ src/component/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+class App extends Component {
+  render(): JSX.Element {
+    return (
+      <div>
+        <h1>Мой апп.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
