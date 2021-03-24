0a54d635ce815d94dc05ea6aeb420645d2e284be
Блинова Екатерина Ивановна <blinova@komus.net>
2021-03-25 01:44:57 +0300

    Add leader board item layout

--- client/src/pages/Sandbox/Sandbox.tsx
+++ client/src/pages/Sandbox/Sandbox.tsx
@@ -3,7 +3,8 @@ import "./Sandbox.scss";
 
 import { Button } from "../../component/Button";
 import { TextField } from "../../component/TextField";
-import { Pagination } from "../../component/Pagination/Pagination";
+import { Pagination } from "../../component/Pagination";
+import { LeaderBoardItem } from "../../component/LeaderBoardItem";
 
 export const SandboxPage = (): JSX.Element => {
   return (
@@ -19,6 +20,24 @@ export const SandboxPage = (): JSX.Element => {
       <TextField label="TextField" placeholder="HelloPlaceholder" />
       <br />
       <Pagination totalRecords={67} />
+      <br />
+      <ul>
+        <LeaderBoardItem
+          rankingPosition={1}
+          playerName="Devon Lane"
+          score={41652}
+        />
+        <LeaderBoardItem
+          rankingPosition={2}
+          playerName="Annette Black"
+          score={36047}
+        />
+        <LeaderBoardItem
+          rankingPosition={3}
+          playerName="Leslie Alexander"
+          score={4432}
+        />
+      </ul>
     </div>
   );
 };
