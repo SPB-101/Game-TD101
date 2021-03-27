7d654b603dd866bae5018677cbcfe792bf5eddfd
Nikita Stroganov <findoss@yandex.ru>
2021-03-28 00:37:43 +0300

    добавил id ключ на странице игры для интеграции

--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -3,5 +3,5 @@ import React from "react";
 import "./Game.scss";
 
 export const GamePage = (): JSX.Element => {
-  return <div className="game">game</div>;
+  return <div className="game" id="GAME-TD-101"></div>;
 };
