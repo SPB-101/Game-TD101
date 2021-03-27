d47ae9d4b3eb2df78e6bdbb27b3661040c62fba7
Nikita Stroganov <findoss@yandex.ru>
2021-03-28 00:33:49 +0300

    добавил звук нажатия кнопки

--- client/src/component/Button/Button.tsx
+++ client/src/component/Button/Button.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import classNames from "classnames/bind";
+
+import clickAudio from "../../assets/sounds/ui_primary/ui_tap-variant-01.wav";
 import * as styles from "./Button.scss";
 
 const cx = classNames.bind(styles);
@@ -11,13 +13,19 @@ export const Button = ({
   children,
   ...props
 }: Props): JSX.Element => {
+  const clickSound = new Audio(clickAudio);
+
   const classNames = cx({
     button: true,
     [`button-${className}`]: true,
   });
 
+  function handleClick() {
+    clickSound.play();
+  }
+
   return (
-    <button className={classNames} {...props}>
+    <button onClick={handleClick} className={classNames} {...props}>
       {children}
     </button>
   );
