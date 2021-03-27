f39aab070b77755d468001026ac2e7ee5a7ad4de
Nikita Stroganov <findoss@yandex.ru>
2021-03-28 00:44:01 +0300

    валидация со старого проекта

---
+++ client/src/utils/validation/index.ts
@@ -0,0 +1,10 @@
+// import {
+//   required,
+//   range,
+//   email,
+//   equalPasswords,
+//   phone,
+// } from './validationRules';
+// export const rules = { required, range, email, equalPasswords, phone };
+
+// export type TRule = (...args: any[]) => string;
---
+++ client/src/utils/validation/validation.ts
@@ -0,0 +1,13 @@
+// import { TRule } from "./index";
+
+// export function validation(value: string | number, rules: TRule[]): string[] {
+//   const arr = rules.map((rule) => rule(value));
+//   if (!arr.some((v) => v !== "")) {
+//     return [];
+//   }
+//   return arr.filter((v) => Boolean(v));
+// }
+
+// export function isValid(errors: string[]): boolean {
+//   return errors.length === 0;
+// }
