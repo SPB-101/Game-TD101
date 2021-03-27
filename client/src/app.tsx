6a506ba27e1c6a2cd2626bc6d5af8df98cdb5f1f
Nikita Stroganov <findoss@yandex.ru>
2021-03-28 00:42:52 +0300

    создал новые роуты

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -4,14 +4,21 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 import { ErrorPage } from "./pages/Error";
 import { SandboxPage } from "./pages/Sandbox";
 import { GamePage } from "./pages/Game";
+import { RegistrationPage } from "./pages/Registration";
+import { LoginPage } from "./pages/Login";
+import { MenuPage } from "./pages/Menu";
 
 import "./styles/root.scss";
+import "./app.scss";
 
 export const App = (): JSX.Element => {
   return (
     <HashRouter>
       <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/registration" component={RegistrationPage} />
         <Route path="/game" component={GamePage} />
+        <Route path="/menu" component={MenuPage} />
         <Route path="/sandbox" component={SandboxPage} />
         <Route component={ErrorPage} />
       </Switch>
