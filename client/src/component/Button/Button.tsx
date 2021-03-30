285c6876d042b4f3dd0b4c5dfca7ac790776a8b7
Nikita Stroganov <findoss@yandex.ru>
2021-03-31 01:35:29 +0300

    исправил кнопку

--- client/src/component/Button/Button.tsx
+++ client/src/component/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 
 import clickAudio from "../../assets/sounds/ui_primary/ui_tap-variant-01.wav";
 import "./Button.scss";
@@ -6,13 +7,20 @@ import "./Button.scss";
 import type { Props } from "./types";
 
 const clickSound = new Audio(clickAudio);
+
 function handleClick() {
   clickSound.play();
 }
 
-export const Button = ({ children, ...props }: Props): JSX.Element => {
+export const Button = ({
+  children,
+  className = "",
+  ...props
+}: Props): JSX.Element => {
+  const buttonClass = classNames("button", className);
+
   return (
-    <button onClick={handleClick} {...props}>
+    <button onClick={handleClick} className={buttonClass} {...props}>
       {children}
     </button>
   );
