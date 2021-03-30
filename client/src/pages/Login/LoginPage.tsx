8b94b0857c2f9c7bee0ced039fac5c44582c93d2
Nikita Stroganov <findoss@yandex.ru>
2021-03-30 16:17:52 +0300

    добавил вход

--- client/src/pages/Login/LoginPage.tsx
+++ client/src/pages/Login/LoginPage.tsx
@@ -8,13 +8,23 @@ import { Wrapper } from "../../component/Wrapper";
 import { TextField } from "../../component/TextField";
 import { Button } from "../../component/Button";
 
+import { signin } from "../../api/auth";
+
 import "./LoginPage.scss";
 
 import { validation } from "../../utils/validation";
 import { required, range } from "../../utils/validation/rules";
 
-const onSubmit = async () => {
-  return { ["FORM_ERROR"]: "Login Failed" };
+const onSubmit = async (values: { [k: string]: string }) => {
+  try {
+    const result = await signin({
+      login: values.login,
+      password: values.password,
+    });
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 const validate = (values: { [k: string]: string }) => {
@@ -26,7 +36,8 @@ const validate = (values: { [k: string]: string }) => {
   };
 
   Object.entries(fields).forEach(([k, v]) => {
-    errors[k] = validation(values[k], v);
+    const err = validation(values[k], v);
+    if (err) errors[k] = err;
   });
 
   return errors;
@@ -41,7 +52,7 @@ export const LoginPage = (): JSX.Element => {
       <Form
         onSubmit={onSubmit}
         validate={validate}
-        render={({ handleSubmit, submitting, values, submitError }) => (
+        render={({ handleSubmit, submitting, submitError }) => (
           <form
             className={classNames("login-page__form", {
               ["login-page__form_error"]: submitError,
@@ -49,7 +60,9 @@ export const LoginPage = (): JSX.Element => {
             onSubmit={handleSubmit}
           >
             {submitError && (
-              <div className="login-page__error-text">{submitError}</div>
+              <div className="login-page__error-text">
+                <span>{submitError}</span>
+              </div>
             )}
             <Field name="login">
               {({ input, meta }) => (
@@ -78,7 +91,7 @@ export const LoginPage = (): JSX.Element => {
               {t("login")}
             </Button>
             <Button disabled={true}>{t("loginYandex")}</Button>
-            <pre>{JSON.stringify(values, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
           </form>
         )}
       />
