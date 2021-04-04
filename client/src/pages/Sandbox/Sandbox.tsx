0a540c39f40af3016e3dacd75537dca40a922696
Nikita Stroganov <findoss@yandex.ru>
2021-04-04 11:42:59 +0300

    Добавил тестовый компонент загрузки картинки

--- client/src/pages/Sandbox/Sandbox.tsx
+++ client/src/pages/Sandbox/Sandbox.tsx
@@ -3,12 +3,18 @@ import "./Sandbox.scss";
 
 import { Button } from "../../component/Button";
 import { TextField } from "../../component/TextField";
+import { ImageField } from "../../component/ImageField";
+import { AvatarField } from "../../component/AvatarField";
 import { Wrapper } from "../../component/Wrapper";
 
 export const SandboxPage = (): JSX.Element => {
   return (
     <Wrapper className="sandbox" size="m">
       SandboxPage
+      <br />
+      <ImageField name="avatar" label="Avatar" />
+      <br />
+      <AvatarField title="Avatar profile" name="avatar" label="Upload image" />
       <br />
       <Button>Button</Button>
       <br />
