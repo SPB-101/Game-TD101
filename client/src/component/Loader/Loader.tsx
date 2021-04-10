309d318d82509a348e512509affbbd38af898942
Nikita Stroganov <findoss@yandex.ru>
2021-04-10 16:32:04 +0300

    добавил задержку на лоадер

--- client/src/component/Loader/Loader.tsx
+++ client/src/component/Loader/Loader.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import classNames from "classnames";
 
 import LoaderIcon from "../../assets/images/icons/loader.svg";
 
+import type { Props } from "./types";
+
 import "./Loader.scss";
 
-export const Loader = (): JSX.Element => {
+export const Loader = ({ delay = 200 }: Props): JSX.Element => {
+  const [view, setView] = useState(true);
+
+  const loaderClass = classNames("loader", {
+    "loader--hide": view,
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => setView(false), delay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className="loader">
+    <div className={loaderClass}>
       <LoaderIcon />
     </div>
   );
