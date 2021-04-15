185cac0c8f78652bf3f22ae841958a682ae656f0
jjharvard <jjharvard@yandex.ua>
2021-04-15 06:28:09 +0300

    Fix locales

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "..", "dist")));
+app.use(express.static(path.join(__dirname, "..", "client/public")));
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
