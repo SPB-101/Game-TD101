dff816df7f060ea4a4444f35c8aea23adf9c634a
jjharvard <jjharvard@yandex.ua>
2021-05-20 17:45:00 +0300

    Add levels

--- client/src/game/Game.ts
+++ client/src/game/Game.ts
@@ -12,7 +12,11 @@ import { AnimatedSprite } from "./model/AnimatedSprite";
 import { GameStat } from "./PanelController";
 
 import { GAME_LOSE, GAME_WAVE_END, GAME_WIN } from "@constants/index";
-import { GameLevel } from "./GameLevel";
+import { GameLevel } from "./level/GameLevel";
+import { GameLevel1 } from "./level/GameLevel1";
+import { GameLevel2 } from "./level/GameLevel2";
+import { GameLevel3 } from "./level/GameLevel3";
+import { GameLevel4 } from "./level/GameLevel4";
 
 export class Game {
   ticks = 0;
@@ -36,10 +40,10 @@ export class Game {
   selected: Turret | null;
   turrets: Turret[] = [];
   levels: GameLevel[] = [
-    new GameLevel(),
-    new GameLevel(),
-    new GameLevel(),
-    new GameLevel(),
+    new GameLevel1(),
+    new GameLevel2(),
+    new GameLevel3(),
+    new GameLevel4(),
   ];
   level: GameLevel;
 
