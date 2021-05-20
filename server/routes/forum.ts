8ca528bb27c9332c3921272048b59f3ee0d7a917
Nikita Stroganov <findoss@yandex.ru>
2021-05-20 11:23:33 +0300

    обновил ручки апи (авторизация)

--- server/routes/forum.ts
+++ server/routes/forum.ts
@@ -1,16 +1,17 @@
 import { Router as createRouter } from "express";
 import { topicsController } from "../controllers/topics";
 import { messagesController } from "../controllers/messages";
+import { protectedAuth } from "../middleware/auth";
 
 import type { Router } from "express";
 
 export const forumRouter = (apiRouter: Router) => {
   const router: Router = createRouter();
-  router.post("/topics/all", topicsController.getTopics);
-  router.post("/topics", topicsController.createTopic);
+  router.post("/topics/all", protectedAuth, topicsController.getTopics);
+  router.post("/topics", protectedAuth, topicsController.createTopic);
 
-  router.post("/topics/:id/all", messagesController.getMessages);
-  router.post("/topics/:id/", messagesController.createMessage);
+  router.post("/topics/:id/all", protectedAuth, messagesController.getMessages);
+  router.post("/topics/:id/", protectedAuth, messagesController.createMessage);
 
   apiRouter.use("/forum", router);
 };
