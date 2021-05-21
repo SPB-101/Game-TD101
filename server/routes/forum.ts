79aa521064a681fc6885f357f21050e883d7e47f
Ekaterina <eblinova@outlook.com>
2021-05-22 02:41:59 +0300

    fix after merge

--- server/routes/forum.ts
+++ server/routes/forum.ts
@@ -5,14 +5,18 @@ import { protectedAuth } from "../middleware/auth";
 
 import type { Router } from "express";
 
-export const forumRouter = (apiRouter: Router) => {
-  const router: Router = createRouter();
+export const forumRouter: Router = createRouter();
 
-  router.get("/topics/all", protectedAuth, topicsController.getTopics);
-  router.post("/topics", protectedAuth, topicsController.createTopic);
+forumRouter.get("/topics/all", protectedAuth, topicsController.getTopics);
+forumRouter.post("/topics", protectedAuth, topicsController.createTopic);
 
-  router.get("/topics/:id/all", protectedAuth, messagesController.getMessages);
-  router.post("/topics/:id", protectedAuth, messagesController.createMessage);
-
-  apiRouter.use("/forum", router);
-};
+forumRouter.get(
+  "/topics/:id/all",
+  protectedAuth,
+  messagesController.getMessages
+);
+forumRouter.post(
+  "/topics/:id",
+  protectedAuth,
+  messagesController.createMessage
+);
