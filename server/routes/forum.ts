641cfb8e5a2fc72f5703afe505816e372dffeeb8
Nikita Stroganov <findoss@yandex.ru>
2021-05-22 00:42:30 +0300

    Update forum.ts

--- server/routes/forum.ts
+++ server/routes/forum.ts
@@ -1,12 +1,8 @@
 import { Router as createRouter } from "express";
 import type { Router } from "express";
 
-export const forumRouter = (apiRouter: Router) => {
-  const router: Router = createRouter();
+export const forumRouter: Router = createRouter();
 
-  router.post("/test", (req, res) =>
-    res.status(200).send(JSON.stringify({ body: req.body, param: req.params }))
-  );
-
-  apiRouter.use("/forum", router);
-};
+forumRouter.post("/test", (req, res) =>
+  res.status(200).send(JSON.stringify({ body: req.body, param: req.params }))
+);
