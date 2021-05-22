54893b6f85d1b539d403482db75a96dcd2f15ada
jjharvard <jjharvard@yandex.ua>
2021-05-23 00:25:03 +0300

    Revert "Merge remote-tracking branch 'origin/main' into develop"
    This reverts commit c6ee11e60c0a19518e08e315792366b2a0426f37, reversing
    changes made to 484b31eb8b62c51d120a38aa1f59ce8a311cc0bc.

--- client/src/component/ThemeProvider/types.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-interface OwnProps {
-  children: React.ReactNode;
-}
-
-export type Props = OwnProps;
--- client/src/pages/LeaderBoardPage/types.ts
+++
@@ -1,8 +0,0 @@
-import { LeaderboardItem } from "@entities/leaderboard/types";
-import { LeaderboardFilter } from "@resolvers/leaderboard/types";
-
-export interface Props {
-  isLoading: boolean;
-  leaderboard: LeaderboardItem[];
-  fetchLeaderboardThunk: (filter: LeaderboardFilter) => Promise<void>;
-}
