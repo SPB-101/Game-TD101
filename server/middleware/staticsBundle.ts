00b592e2b051972fa822319b4a716ba3bc426640
Nikita Stroganov <findoss@yandex.ru>
2021-05-31 22:38:58 +0300

    исправил раздачу статики для прода (#67)

--- server/middleware/staticsBundle.ts
+++ server/middleware/staticsBundle.ts
@@ -1,13 +1,8 @@
 import path from "path";
 import express from "express";
-import type * as core from "express-serve-static-core";
 
 const rootDir = process.cwd();
-import { IS_DEV } from "../../constants/server";
 
 export const staticsBundle = () => {
-  if (IS_DEV) {
-    return express.static(path.join(rootDir, "dist"));
-  }
-  return (req, res, next) => next();
+  return express.static(path.join(rootDir, "dist"));
 };
--- server/middleware/staticsPublic.ts
+++ server/middleware/staticsPublic.ts
@@ -2,11 +2,7 @@ import path from "path";
 import express from "express";
 
 const rootDir = process.cwd();
-import { IS_DEV } from "../../constants/server";
 
 export const staticsPublic = () => {
-  if (IS_DEV) {
-    return express.static(path.join(rootDir, "client/public"));
-  }
-  return (req, res, next) => next();
+  return express.static(path.join(rootDir, "client/public"));
 };
