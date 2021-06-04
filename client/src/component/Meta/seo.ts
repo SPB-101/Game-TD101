fe85e9b84811c0dc2bdab8bcc5027a9853a13e57
Nikita Stroganov <findoss@yandex.ru>
2021-06-05 00:11:53 +0300

    Update seo.ts

--- client/src/component/Meta/seo.ts
+++ client/src/component/Meta/seo.ts
@@ -1,5 +1,8 @@
+import banner from "./banner.png";
+import { HOST } from "@constants/index";
+
 export const data = {
   description: "Cool meh style tower defense! Play now!",
   title: "TD 101",
-  image: "./banner.png",
+  image: HOST + banner.slice(1),
 };
